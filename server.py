058e2517850e35a6d72940c13642f4de54cfd4f1
Lee Campbell <campbel2@tcd.ie>
2019-02-14 13:44:18 +0000

    opening socket fixed

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-import socket, thread
+import socket
 
 host = '' # localhost(blank)
 port = 8080 # alt-http-port
 
-  try:
+try:
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create Socket
     s.bind((host, port)) # Bind socket to a host and port
 
-except socket.error as err, (value, message):
+except socket.error as err:
         if s:
             s.close()
-        print "Socket could not be opened", message
+        print ("Socket could not be opened")
         sys.exit(0)
